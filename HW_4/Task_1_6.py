eff61f29ba140c01f9a8e0b7b90a25dc4630a4b2
Marat <email.marat@gmail.com>
2023-06-16 17:51:23 +0300

    fixed Task_1_6

--- HW_4/Task_1_6.py
+++ HW_4/Task_1_6.py
@@ -4,18 +4,21 @@
 # до конца и/или начала списка.
 
 def sum_list(a, b, args):
-    sum_i = 0
+    # sum_i = 0
+    if a > b:
+        a, b = b, a
     if a < 0:
         a = 0
     if b > len(args)-1:
         b = len(args)-1
-    for i in range(a, b+1):
-        sum_i += args[i]
+    # for i in range(a, b+1):
+    #     sum_i += args[i]
+    sum_i = sum(args[a:b+1])
     return sum_i
 
 
 list_of_nums = list(map(int, input('Введите список чисел через пробел: ').split()))
-start_index = int(input('Начальный индекс: '))
-end_index = int(input('Конечный индекс: '))
+start_index = int(input('Первый индекс: '))
+end_index = int(input('Второй индекс: '))
 
 print(sum_list(start_index, end_index, list_of_nums))
