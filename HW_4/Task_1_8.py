6d3aa8c00ccdb0b48044ac32ce1319985d42142d
Marat <email.marat@gmail.com>
2023-06-16 19:04:43 +0300

    fixed Task_1_8

--- HW_4/Task_1_8.py
+++ HW_4/Task_1_8.py
@@ -3,20 +3,16 @@
 # оканчивающихся на s (кроме переменной из одной буквы s) на None.
 # ✔ Значения не удаляются, а помещаются в одноимённые переменные без s на конце.
 
-def rename_l(alist):
-    new_list = []
-    for i in range(len(alist)):
-        n, m = alist[i].split('=')
-        if len(n)>1 and n[-1] == 's':
-            new_list.append(n + '=' + 'None')
-            new_per = n[:-1] + '=' + m
-            new_list.append(new_per)
-        else:
-            new_list.append(alist[i])
-    return new_list
+def rename_l(**alist):
+    second_dict = {}
+    for k, v in alist.items():
+        if len(k)>1 and k[-1] == 's':
+             new_k = k[:-1]
+             second_dict[new_k] = v
+             alist[k] = None
+    alist.update(second_dict)
+
+    return alist
 
 
-list_per = ['as=456', 'word=446', 's="sssg"', 'names=4456', 'cars=345', 'squart=34556']
-
-for i in rename_l(list_per):
-    print(i)
+print(rename_l(ask=456, word=446, s="sssg", names=4456, cars=345, squart=34556))
